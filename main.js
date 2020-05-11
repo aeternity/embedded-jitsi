4ad2e6594fc8c427bae470eec6e4cc49dc535252
Dincho Todorov <dincho@users.noreply.github.com>
2020-05-11 17:49:40 +0300

    Embedded page configuration

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
-const domain = 'league.superhero.com';
+const params = (new URL(location)).searchParams;
+const roomName = params.has('roomName') ? params.get('roomName') : '';
+const domain = 'league.aeternity.org';
+
 const options = {
-    roomName: 'JitsiMeetAPIExample',
+    roomName: roomName,
     width: '100%',
     height: '100%',
     parentNode: document.querySelector('#meet'),
+    noSSL: false,
+    configOverwrite: {
+        enableNoAudioDetection: true,
+        enableNoisyMicDetection: true,
+        startWithAudioMuted: true,
+        startWithVideoMuted: true,
+
+        fileRecordingsEnabled: true,
+        liveStreamingEnabled: true,
+        channelLastN: 8,
+        useStunTurn: true,
+
+        openBridgeChannel: 'websocket',
+        requireDisplayName: true
+    },
+
     // more interface option here: https://github.com/jitsi/jitsi-meet/blob/master/interface_config.js
     interfaceConfigOverwrite: {
-        SHOW_JITSI_WATERMARK: false,
+        TOOLBAR_ALWAYS_VISIBLE: true,
         SHOW_BRAND_WATERMARK: false,
         VERTICAL_FILMSTRIP: true,
-        RECENT_LIST_ENABLED: false,
-        SHOW_POWERED_BY: true,
-        filmStripOnly: false,
+        DISABLE_JOIN_LEAVE_NOTIFICATIONS: true,
 
-    },
+        CHAT_ALWAYS_OPEN: false,
+        TILE_VIEW_ALWAYS_OPEN: false,
+
+        startWithAudioMuted: true,
+        startWithVideoMuted: true
+    }
 };
 
 const api = new JitsiMeetExternalAPI(domain, options);
-
-api.executeCommand('subject', 'Aeternity test conference');
-api.executeCommand('email', 'example@aeternity.com');
